a89e6696259d6245af37a427123dc97395280279
Albertino Neto <albeneto72@gmail.com>
2021-09-09 20:06:42 -0300

    Limpando os campos e setando foco na data

--- client/app/index.js
+++ client/app/index.js
@@ -23,4 +23,13 @@ document.querySelector('.form').addEventListener('submit', function(event){
     tdVolume.textContent = campos[1].value * campos[2].value;
     tr.appendChild(tdVolume);
     tbody.appendChild(tr);
+
+    // limpa o campo data
+    campos[0].value = '';
+    // limpa o campo quantidade
+    campos[1].value = 1;
+    // limpa o campo valor
+    campos[2].value = 0.0;
+    // muda o foco para o campo data
+    campos[0].focus();
 });
