bb7fc38136211ce3b8de4331662f9fba27f4db13
Albertino Neto <albeneto72@gmail.com>
2021-09-17 19:13:41 -0300

    Adicionando mensagem e refatorando as views

--- client/app/ui/views/NegociacoesView.js
+++ client/app/ui/views/NegociacoesView.js
@@ -1,13 +1,5 @@
-class NegociacoesView{
+class NegociacoesView extends View {
     
-    constructor(seletor){
-        this._elemento = document.querySelector(seletor);
-    }
-
-    update(model) {
-        this._elemento.innerHTML = this.template(model);
-    }
-
     template(model) {
 
         return `
@@ -43,7 +35,5 @@ class NegociacoesView{
                
             </table>
         `;
-
-
     }
 }
