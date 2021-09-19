803ee96ed3828495a5586e78a32d8db0e444a805
Albertino Neto <albeneto72@gmail.com>
2021-09-18 21:47:30 -0300

    Padrão Factory

---
+++ client/app/util/ProxyFactory.js
@@ -0,0 +1,25 @@
+class ProxyFactory {
+    static create(objeto, props, armadilha) {
+
+       return new Proxy(objeto, {
+
+            get(target, prop, receiver){
+
+                if(typeof(target[prop]) == typeof(Function) && props.includes(prop)) {
+ 
+                    return function() {
+                        console.log(`"${prop}" disparou a armadilha`);
+                        target[prop].apply(target, arguments);
+                        armadilha(target)
+                    }
+
+                } else {
+                    // realizando um get padrão
+                    return target[prop];
+                }
+            }
+        });
+
+
+    }
+}
