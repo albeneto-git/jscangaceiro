6e8dd4aba3c268a567ad5158a2eca9b49892490b
Albertino Neto <albeneto72@gmail.com>
2021-09-21 21:35:37 -0300

    Criando conexão unica para aplicação

---
+++ client/app/util/ConnectionFactory.js
@@ -0,0 +1,53 @@
+/*
+    Premissas:
+    O metodo getConnection() sempre retorna a mesma conexão para a aplicação toda.
+    Toda conexão tem um metodo close(), porém não pode ser chamado, pois a conexão é única para toda a aplicação.
+    Só esta fabrica pode fechar a conexão.
+*/
+
+const stores = ['negociacoes'];
+
+let connection = null;
+
+class ConnectionFactory {
+    constructor() {
+        throw new Error('Não é possível criar instâncias dessa classe');
+    }
+
+    static getConnection() {
+        
+        const stores = ['negociacoes'];
+        
+        return new Promise((resolve, reject) => {
+
+            if(connection) return resolve(connection);
+
+            const openRequest = indexedDB.open('jscangaceiro', 2);
+
+            openRequest.onupgradeneeded = e => {
+                ConnectionFactory._createStores(e.target.result);
+            };
+
+            openRequest.onsuccess = e => {
+                connection = e.target.result;
+                resolve(e.target.result);
+            };
+
+            openRequest.onerror = e => {
+                console.log(e.target.error)
+                reject(e.target.error.name)
+            };
+        });
+    }
+
+    static _createStores(connection) {
+        stores.forEach(store => {
+            // if sem bloco, mais sucinto!
+            if(connection.objectStoreNames.contains(store))
+                connection.deleteObjectStore(store);
+
+            connection.createObjectStore(store, { autoIncrement: true });
+        });
+    }
+
+}
