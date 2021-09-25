0a35b48c6dac891081c5ed04a329e3b924e4d643
Albertino Neto <albeneto72@gmail.com>
2021-09-25 19:37:46 -0300

    Apaga todas as negociações

--- client/app/domain/negociacao/NegociacaoDao.js
+++ client/app/domain/negociacao/NegociacaoDao.js
@@ -52,4 +52,22 @@ class NegociacaoDao {
 
         });
     }
+
+    apagaTodas(){
+        return new Promise((resolve, reject) =>{
+
+            const request = this._connection
+                .transaction([this._store], 'readwrite')
+                .objectStore(this._store)
+                .clear();
+            
+            request.onsuccess = e => resolve();
+
+            request.onerror = e => {
+                console.log(e.target.error);
+                reject('Não foi possível apagar as negociações');
+            };
+
+        });
+    }
 }
